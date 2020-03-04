ada34bca08b4e2c3bba3c2523929b477ff2ddb02
gungupeas <silenta1moves@gmail.com>
2020-03-04 16:39:33 -0600

    cart broke

--- src/store.js
+++ src/store.js
@@ -13,10 +13,11 @@ const store = createStore(
 	rootReducer,
 	compose(
 		applyMiddleware(...middleware),
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+		? a => a
+		: window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+		// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 	)
 )
-
-// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 export default store
